0203dcc350e3e4d2c00b1d35a31f74fcfc73bba8
Aleksey Korotkov <AVKorotkov@users.noreply.github.com>
2016-05-14 15:42:19 +0400

    Kile2

--- kile.sh
+++ kile.sh
@@ -3,6 +3,6 @@
 export DEBIAN_FRONTEND=noninteractive
 
 aptitude install kile kile-doc -yq
-aspell aspell-en aspell-fr aspell-ru -yq
-hunspell hunspell-en-us hunspell-fr hunspell-fr-classical hunspell-ru hunspell-tools -yq
+aptitude install aspell aspell-en aspell-fr aspell-ru -yq
+aptitude install hunspell hunspell-en-us hunspell-fr hunspell-fr-classical hunspell-ru hunspell-tools -yq
 aptitude install ispell -yq
